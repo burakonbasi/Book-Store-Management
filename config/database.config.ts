f0882c724c2f9147a565c28a83961c354e28873d
Burak Onbaşıoğlu <burakonbasioglu@gmail.com>
2024-05-02 13:33:33 +0300

    Dosyalar Update

--- config/database.config.ts
+++ config/database.config.ts
@@ -1,16 +1,14 @@
-// config/database.config.ts
-
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost', 
-  port: 5432, 
-  username: 'your_username',
-  password: 'your_password', 
-  database: 'bookstore', 
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'], 
-  synchronize: true, 
+  host: 'localhost',
+  port: 5432,
+  username: 'bookstore_postgres',
+  password: 'Burak1995',
+  database: 'bookstore',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
 };
 
 export default typeOrmConfig;
